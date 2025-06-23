bc3a0822326161eb64eb51075636d11ca18db406
yuvarajuboddu <yuvaraju.boddu@gmail.com>
2025-06-23 15:25:38 -0700

    updated arraylist class

--- mavencorejava/src/main/java/CollectionFrameWorks/ArrayListExample.java
+++ mavencorejava/src/main/java/CollectionFrameWorks/ArrayListExample.java
@@ -4,6 +4,15 @@ import java.util.*;
 public class ArrayListExample {
 	public static void main(String[] args) {
 		ArrayList<Integer> num = new ArrayList<>();
+		try {
+			num.add(1);
+			num.add(2);
+			num.add(5);
+			System.out.println(num);
+		}catch(Exception e) {
+			System.out.println("This is catch block");
+			
+		}
 	}
 
 }
