08fdf722abd1b5c05dd59d672b8ce06c701f77a8
yuvarajuboddu <yuvaraju.boddu@gmail.com>
2025-06-23 14:20:28 -0700

    core java files

---
+++ mavencorejava/src/main/java/CollectionFrameWorks/ArrayListExample.java
@@ -0,0 +1,9 @@
+package CollectionFrameWorks;
+import java.util.*;
+
+public class ArrayListExample {
+	public static void main(String[] args) {
+		ArrayList<Integer> num = new ArrayList<>();
+	}
+
+}
