410e6e5a12ef183b5545296639243aadc1f1fc9d
yuvarajuboddu <yuvaraju.boddu@gmail.com>
2025-07-01 15:27:28 -0700

    String methods

---
+++ mavencorejava/src/main/java/CollectionFrameWorks/StringExample.java
@@ -0,0 +1,23 @@
+package CollectionFrameWorks;
+
+public class StringExample {
+	public static void main(String[] args) {
+		String str1 ="corejava";
+		String str2 ="Interview";
+		
+		int length =str1.length();
+		System.out.println("Lemgth og String 1: "+length);
+		
+		String result =str1.concat(","+str2);
+		System.out.println(result);
+		
+		String subString = result.substring(4,8);
+		System.out.println("SubString of index(4,8) :" +subString);
+		
+		int find = result.indexOf('v');
+		System.out.println("find the char by using index value: "+find);
+		
+		boolean value = str1.equals(str2);
+		System.out.println(value);
+	}
+}
